c96d62f831942dad602b18dd6160d4537cf265b7
Anis Boutouria <enisboutouria5@gmail.com>
2021-07-19 01:37:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import style from './style.css';
 import './App.css';
+import image1 from './Image/image1.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" 
+    style={{border:'solid 1',color:'black',maxWidh:'100vw'}}>
+      <h1 className="app-title" > Anis Boutouria </h1>
+   
+     <img src="/image2.png" alt="image from public"/>
+     <br/>
+     <img src= {image1}  alt="image from src"/>
+     <br/>
+     <video width={320} height={240} controls>
+     <source src="myVideo.mp4" type="video/mp4" />
+     </video>
     </div>
   );
 }
